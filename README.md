16735231068d761d48123a295c31e3f45bc5ce46
rp-bot_win10 <legacy.business18@gmail.com>
2023-04-04 23:02:53 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# NextJS Automatic App creator
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nextpy",
+    version="0.0.1",
+    author="rp-bot",
+    author_email="prathamvadhulas@gmail.com",
+    description=("Python program that automates creating a nextjs app "),
+    long_description="jkshfdgvkhjfsdbvkjh",
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'nextpy=nextpy.default_app:main',
+        ],
+    },
+)
