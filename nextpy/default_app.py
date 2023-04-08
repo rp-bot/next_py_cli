a51454f82715b57479218e116167d4e16413af12
rp-bot_win10 <legacy.business18@gmail.com>
2023-04-07 23:26:08 -0400

    pyproject.toml added

--- nextpy/default_app.py
+++ nextpy/default_app.py
@@ -4,17 +4,8 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='A simple CLI program.')
-    parser.add_argument('name', help='Your name')
-    parser.add_argument('age', type=int, help='Your age')
-    parser.add_argument(
-        '-g', '--gender', choices=['male', 'female'], help='Your gender')
-    args = parser.parse_args()
-    print(f'Hello, {args.name}!')
-    if args.age:
-        print(f'You are {args.age} years old.')
-    if args.gender:
-        print(f'You are {args.gender}.')
+    parser = argparse.ArgumentParser(
+        description='Another unnecessary level of automation.')
 
 
 if __name__ == '__main__':
