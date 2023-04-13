edd32119495d631bf25d3959fea14b02c2caa153
rp-bot_win10 <legacy.business18@gmail.com>
2023-04-12 22:16:27 -0400

    working

--- nextpy/default_app.py
+++ nextpy/default_app.py
@@ -5,12 +5,13 @@ import subprocess
 RP_BOT_COMMAND = """npx create-next-app@latest example6 --js --tailwind --eslint --src-dir --no-experimental-app  --import-alias "@/*"""
 
 
-def command_maker(project_name, lang_name, tailwind, eslint, experimental_app, src_dir, import_alias, alias_string, bootstrap_client, help, create_command="create-next-app@latest", version=None):
-    if version != None:
-        create_command = "create-next-app"
+def command_maker(project_name, lang_name, tailwind, eslint, experimental_app, src_dir, import_alias, alias_string, bootstrap_client, help, create_command="create-next-app@latest", version=""):
+    if version != "":
+        create_command = "create-next-app@latest"
+    else:
+        create_command+version
     command = ["npx",
                create_command,
-               version,
                project_name,
                lang_name,
                tailwind,
@@ -28,12 +29,12 @@ def main():
 
     parser = argparse.ArgumentParser(
         description='Another unnecessary level of automation.')
-    args = parser.parse_args()
+
     # ProjectName
     parser.add_argument("project_name", type=str,
                         help="Type the name of the nextjs project. This will also be the name of your folder.")
     # Type script or javascript
-    parser.add_argument("--ts", "--typescript", type=str, default="--js",
+    parser.add_argument("--typescript", "--ts", type=str, default="--js",
                         help="Specify if you WANT typescript. Default is javascript")
 
     # Tailwind or no Tailwind
@@ -57,19 +58,15 @@ def main():
     # Alias string
     # TODO: Do a condition statement to check whether alias is selected or not.
     alias_string_arg = None
-    if args.no_import_alias == "--import-alias":
-        parser.add_argument("alias_string_arg", type=str, default="@/*",
-                            help="Specify if you DON'T WANT the import alias. Default is it imports alias")
 
     # bootstrapclient arg
     parser.add_argument("--use-pnpm", type=str, default="--use-npm",
                         help="Specify if you WANT Pnpm. Default is npm")
 
-    parser.add_argument("--next-help", type=str, default=None,
+    parser.add_argument("--next-help", type=str, default="",
                         help="Next JS help")
-
+    args = parser.parse_args()
     # VERSION
-
     project_name = args.project_name
     language = args.typescript
     tailwind = args.no_tailwind
@@ -77,13 +74,13 @@ def main():
     src_dir = args.no_src_dir
     experimental_app = args.experimental_app
     import_alias = args.no_import_alias
-    alias_string = args.alias_string_arg
+    alias_string = "@/*"
     bootstrap_client = args.use_pnpm
     help_arg = args.next_help  # needs to be converted to --help from --next-help
-    command_maker(project_name, language, tailwind, eslint, src_dir,
-                  experimental_app, import_alias, alias_string, bootstrap_client, help_arg, )
-    x = subprocess.Popen([],
-                         shell=True, text=True)
+    command = command_maker(project_name, language, tailwind, eslint, src_dir,
+                            experimental_app, import_alias, alias_string, bootstrap_client, help_arg)
+    print(command)
+    x = subprocess.Popen(command, shell=True, text=True)
     x.wait()
 
 
