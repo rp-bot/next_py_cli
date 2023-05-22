a634da2f76e0469d1f9d1674887ebce09dc951a4
rp-bot_win10 <legacy.business18@gmail.com>
2023-05-22 13:51:02 -0400

    working cli

--- nextpy/default_app.py
+++ nextpy/default_app.py
@@ -8,18 +8,52 @@ OS = platform.system()
 
 
 def command_maker(argument_dict):
-    # TODO: classify the essential and non essential arguments to catch errors. 
-    # non_esential=["language", ] 
+    # TODO: classify the essential and non essential arguments to catch errors.
+    # non_esential=["language", ]
     command = []
     for value in argument_dict.values():
-        if value is not None: command.append(value)
+        if value is not None:
+            command.append(value)
     return command
 
-def clean_up(project_name):
-# clean up files in the directory
-    with open(f"{project_name}/tailwind.config.js","w+") as tailwind_config_js, open(f"{project_name}/src/styles/globals.css","w+") as globals_css, open(f"{project_name}/src/pages/index.js","w+") as index_js:
-        tailwind_config_js.writelines(
-            """/** @type {import('tailwindcss').Config} */
+
+def clean_up(project_name: str, app_dir: str):
+    # clean up files in the directory
+    if app_dir == "--app":
+        with open(f"{project_name}/tailwind.config.js", "w+") as tailwind_config_js, open(f"{project_name}/src/app/globals.css", "w+") as globals_css, open(f"{project_name}/src/app/page.js", "w+") as index_js:
+            tailwind_config_js.writelines(
+                """/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}""")
+            globals_css.writelines(
+                """@tailwind base;
+@tailwind components;
+@tailwind utilities;"""
+            )
+            index_js.writelines(
+                """export default function Home() {
+	return (
+		<div className="bg-black">
+			<div>Home Page</div>
+		</div>
+	);
+}"""
+            )
+
+    elif app_dir == "--no-app":
+        with open(f"{project_name}/tailwind.config.js", "w+") as tailwind_config_js, open(f"{project_name}/src/styles/globals.css", "w+") as globals_css, open(f"{project_name}/src/pages/index.js", "w+") as index_js:
+            tailwind_config_js.writelines(
+                """/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -33,38 +67,49 @@ module.exports = {
   plugins: [],
 }""")
 
-        globals_css.writelines(
-            """@tailwind base;
+            globals_css.writelines(
+                """@tailwind base;
 @tailwind components;
 @tailwind utilities;"""
-        )
+            )
 
-        index_js.writelines(
-            """export default function Home() {
+            index_js.writelines(
+                """export default function Home() {
 	return (
 		<div className="bg-black">
 			<div>Home Page</div>
 		</div>
 	);
 }"""
-        )
-    
+            )
+
     # FIXME this is os specific need to make it universal
-    x = subprocess.Popen(['cmd', '/c', 'del', f'.\\{project_name}\\public\\*.svg', ], shell=True, text=True,cwd=CWD)
-    x.wait()
+    delete_process = subprocess.Popen(
+        ['cmd', '/c', 'del', f'.\\{project_name}\\public\\*.svg', ], shell=True, text=True, cwd=CWD)
+    delete_process.wait()
 
-def auto_start_app(project_name,no_auto_arg=None):
+
+def auto_start_app(project_name, no_auto_arg=None):
+    # change to specified project dir
+    os.chdir(os.path.join(project_name))
+
+    open_IDE = subprocess.Popen(
+        ['code', f'.\\{project_name}', ], shell=True, text=True, cwd=CWD)
+    open_IDE.wait()
+
     if no_auto_arg is None:
-        # change to specified project dir
-        os.chdir(os.path.join(project_name))
-
-        install_command = subprocess.Popen(['npm','install'], shell=True, text=True,cwd=os.getcwd())
+        # Start app
+        install_command = subprocess.Popen(
+            ['npm', 'install'], shell=True, text=True, cwd=os.getcwd())
         install_command.wait()
 
-        run_command = subprocess.Popen(['npm','run', 'dev'], shell=True, text=True,cwd=os.getcwd())
+        run_command = subprocess.Popen(
+            ['npm', 'run', 'dev'], shell=True, text=True, cwd=os.getcwd())
         run_command.wait()
 
-    else: print("App was created and then wasn't started")
+    else:
+        print("App was created, but wasn't started")
+
 
 def main():
 
@@ -90,7 +135,7 @@ def main():
                         help="Specify if you DON'T WANT the src-dir. Default is WITH src-dir")
 
     # experimental app or no
-    parser.add_argument("--experimental-app", type=str, default="--no-experimental-app",
+    parser.add_argument("--no-app", type=str, default="--app",
                         help="Specify if you WANT the experimental-app. Default is WITHOUT experimental-app")
 
     # import alias or nah
@@ -103,48 +148,43 @@ def main():
 
     parser.add_argument("--next-help", type=str, default=None,
                         help="Next JS help")
-    
+
     parser.add_argument("--no-auto-start", type=str, default=None,
                         help="Automatically start development server")
 
     args = parser.parse_args()
     # VERSION
-    main_arguments ={
+    main_arguments = {
         "package_execution": "npx",
         "create_command": "create-next-app@latest",
         "version": None,
-        "project_name":args.project_name,
-        "language":args.typescript,
-        "tailwind":args.no_tailwind,
-        "eslint":args.no_eslint,
-        "src_dir" : args.no_src_dir,
-        "experimental_app" : args.experimental_app,
-        "import_alias" : args.no_import_alias,
+        "project_name": args.project_name,
+        "language": args.typescript,
+        "tailwind": args.no_tailwind,
+        "eslint": args.no_eslint,
+        "src_dir": args.no_src_dir,
+        "no_app": args.no_app,
+        "import_alias": args.no_import_alias,
         # TODO: Make Alias string customizable
-        "alias_string" : "@/*",
-        "bootstrap_client" : args.use_pnpm,
-        
+        "alias_string": "@/*",
+        "bootstrap_client": args.use_pnpm,
+
     }
-    other_arguments = {"no_auto":args.no_auto_start}
-    
+    other_arguments = {"no_auto": args.no_auto_start}
 
     #  construct command
     main_command = command_maker(main_arguments)
 
-    
     # run command
     x = subprocess.Popen(main_command, shell=True, text=True)
     x.wait()
-    
+
     # Run clean up function if app was created or else dont
-    clean_up(main_command[2]) if not x.poll() else None
+    clean_up(main_command[2], main_command[7]) if not x.poll() else None
 
     # Start app
-    auto_start_app(main_command[2]) 
-    
-
+    auto_start_app(main_command[2])
 
 
 if __name__ == '__main__':
     main()
-    
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nextpy",
-    version="0.0.3",
+    version="1.0.1",
     author="Pratham Vadhulas",
     description=("Python CLI that automates creating a nextjs app"),
     long_description="""The Next.js App is created with a CLI. This wrapper is a Python program that helps you create a new Next.js app with less hassle. Say goodbye to boilerplate code, and hello to a clean start. Just enter your project name and the CLI will do the rest.""",
